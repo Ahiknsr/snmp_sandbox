c80fdb2a0e7668344051250d8e995dc47575d4c8
Ian Kronquist <iankronquist@osuosl.org>
2014-05-13 14:27:11 -0700

    Change Controller object

--- controller.py
+++ controller.py
@@ -1,20 +1,24 @@
+import atexit
 import curses_cli
 import db_model
 import pdumaster
 
 class Controller:
-    def setup():
+    def setup(self):
         """Setup ui and database, get pdu stats."""
-        pdumaster.start_ui()
+        self.cli = curses_cli.curses_cli()
+        self.cli.setup()
+        pdus = ('a', 'b')
+        self.cli.draw_ui(pdus)
         #When the program exits call the cleanup to clean up curses and the db
-        atexit.register(cleanup)
-        db_model.initdb(in_memory=True)
-        db_model.get_stats(pdumaster.get_stats())
+        atexit.register(self.cleanup)
+        #db_model.initdb(in_memory=True)
+        #db_model.get_stats(pdumaster.get_stats())
 
-    def cleanup():
+    def cleanup(self):
         """When the program exits clean up changes curses made to the terminal and the db session"""
-        curses_cli.cleanup()
-        db_model.cleanup()
+        self.cli.cleanup()
+        #db_model.cleanup()
 
 if __name__ == '__main__':
     controller = Controller()
