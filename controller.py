9db032f555682d8f9689e353f0d40ac94d81762b
Ian Kronquist <iankronquist@osuosl.org>
2014-05-15 12:14:53 -0700

    Reorganize pdu whisperer calls

--- controller.py
+++ controller.py
@@ -1,4 +1,6 @@
+import json
 import atexit
+
 import curses_cli
 import db_model
 import pdumaster
@@ -6,14 +8,29 @@ import pdumaster
 class Controller:
     def setup(self):
         """Setup ui and database, get pdu stats."""
-        self.cli = curses_cli.curses_cli()
-        self.cli.setup()
-        pdus = ('a', 'b')
-        self.cli.draw_ui(pdus)
         #When the program exits call the cleanup to clean up curses and the db
         atexit.register(self.cleanup)
+
+        self.cli = curses_cli.curses_cli()
+        self.cli.setup()
+
+        pdus = read_config()
+        print pdus
+
+        whisperer = pdu_device_whisperer(pdus)
+
+        self.cli.draw_ui(pdus)
         self.db = db_model.db_model(inMemory=True)
         self.db.add_many_pdus(pdumaster.get_stats())
+
+    def read_config(config_name=None):
+        """Reads a config file (default is ./osuosl_pdus.conf), parses it as json and returns the python dictionary it represents"""
+        if config_name != None:
+            controller = './osuosl_pdus.conf'
+        config = open(controller, 'r')
+        config_pdus = json.load(config)
+        config.close()
+        return  config_pdus
 
 
     def cleanup(self):
