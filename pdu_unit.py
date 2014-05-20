58a4d02b4f764f3b0b49d4b71dd50bab4a95a424
Ian Kronquist <iankronquist@osuosl.org>
2014-05-20 13:27:38 -0700

    Split pdu_unit into its own file

---
+++ pdu_unit.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.declarative import declarative_base as sql_base
+
+Base = sql_base()
+
+class pdu_unit_status(Base):
+    """This class represents a table in the database holding pdu data"""
+    __tablename__ = 'pdus'
+    pdu_name = Column(String, primary_key=True)
+    pdu_type = Column(String, primary_key=True)
+    label = Column(String)
+    group = Column(String)
+
+    def __repr__(self):
+        """For pretty printing"""
+        return "pdu_name %s, sym_name %s, suffix %s, val %i" % (self.name, self.fullname, self.password)
