ff388bee8e6ac97bd76509f4e8e9c633838abea8
Ian Kronquist <iankronquist@osuosl.org>
2014-05-20 13:26:23 -0700

    Move pdu_unit class into its own file

--- db_model.py
+++ db_model.py
@@ -1,28 +1,13 @@
-from sqlalchemy.ext.declarative import declarative_base as sql_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
 from sqlalchemy import update
 from sqlalchemy.orm import sessionmaker
-
-class pdu_unit_status(sql_base()):
-    """This class represents a table in the database holding pdu data"""
-    __tablename__ = 'pdus'
-    pdu_name = Column(String, primary_key=True)
-    sym_name = Column(String)
-    suffix = Column(String, primary_key=True)
-    val = Column(Integer)
-
-    def __repr__(self):
-        """For pretty printing"""
-        return "pdu_name %s, sym_name %s, suffix %s, val %i" % (self.name, self.fullname, self.password)
-
-
 class db_model():
     def __init__(self, in_memory=True):
         """Initialize database connection and possibly create the database in memory"""
         if in_memory:
             self.engine = create_engine("sqlite:///:memory:")
-            sql_base.metadata.create_all(self.engine)
+            Base.metadata.create_all(self.engine)
         else:
             # The database is assumed to already exist at ./pdumaster.db
             self.engine = create_engine("sqlite://pdumaster.db")
