61f5dd47f6d2dbcfa8074ea1937f55a8dc5f1cb6
Ian Kronquist <iankronquist@osuosl.org>
2014-05-20 14:53:24 -0700

    Refactor; add a last updated column

--- pdu_model.py
+++ pdu_model.py
@@ -2,13 +2,14 @@ from sqlalchemy.ext.declarative import declarative_base as sql_base
 
 Base = sql_base()
 
-class pdu_unit_status(Base):
+class pdu_unit_model(Base):
     """This class represents a table in the database holding pdu data"""
     __tablename__ = 'pdus'
     pdu_name = Column(String, primary_key=True)
     pdu_type = Column(String, primary_key=True)
     label = Column(String)
     group = Column(String)
+    last_updated = Column()
 
     def __repr__(self):
         """For pretty printing"""
