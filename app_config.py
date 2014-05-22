07f6f42b4cf84a2899b33a0c1f02236072ee4b74
Ian Kronquist <iankronquist@osuosl.org>
2014-05-21 17:23:45 -0700

    Minor refactoring

--- app_config.py
+++ app_config.py
@@ -7,20 +7,20 @@ db_engine = None
 #This is the location of the application config file. it should be a json file
 config_file_name = "pdumaster.json"
 
-log_level = "debug"
+app_log_level = "debug"
 
-log_file = "debug.log"
+app_log_file = "debug.log"
 
 def read_pdumaster_config():
     """Read in the application configuration from pdumaster.json."""
     global config_file_name
     global db_engine
-    global log_level
-    global log_file
+    global app_log_level
+    global app_log_file
     config = open(config_file_name, 'r')
     config_dict = json.load(config)
     config.close()
     db_engine = config_dict.get('db_engine')
-    log_level = config_dict.get('log_level')
-    log_file = config_dict.get('log_file')
+    app_log_level = config_dict.get('log_level')
+    app_log_file = config_dict.get('log_file')
 
