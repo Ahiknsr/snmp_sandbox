ba51e63e59a26bd792bd93cb094a678e99918716
Ian Kronquist <iankronquist@osuosl.org>
2014-05-28 15:06:14 -0700

    Make Pdu Whisperer a class

--- pdu_whisperer.py
+++ pdu_whisperer.py
@@ -1,20 +1,22 @@
-from collections import namedtuple
+#thanks checkaa for the name
 
 from pysnmp import debug
 #from pysnmp.smi import builder, view
 from pysnmp.entity.rfc3413.oneliner import cmdgen
 
+import pdu_exceptions
 
-pdu_data = namedtuple("pdu_data", ("modName", "symName", "suffix"))
+class PduWhisperer(object):
 
-#thanks checkaa for the name
-class pdu_whisperer:
-    def __init__(self, pdus):
-        self.pdus = pdus
+    port = 161
 
-    def scan_pdus(self):
+    def __init__(port=None):
+        if port != None:
+            self.port = port
+
+    def scan_pdus(pdus):
         """Scans all the pdus and returns a list of named tuples"""
-        for pdu in self.pdus:
+        for pdu in pdus:
             errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.nextCmd(
                 cmdgen.CommunityData('OSL_private'),
                 cmdgen.UdpTransportTarget((pdu, port)),
@@ -26,15 +28,10 @@ class pdu_whisperer:
         pdu_data_list = []
         # Check for errors and print out results
         if errorIndication:
-            #TODO: handle errors gracefully
-            print(errorIndication)
-            error += die
+            raise pdu_exceptions.SnmpActivationError(errorStatus, errorIndex)
         else:
             if errorStatus:
-                print('%s at %s' % (errorStatus.prettyPrint(),
-                errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'))
-                #TODO: handle errors gracefully
-                error += die
+                raise pdu_exceptions.SnmpActivationError(errorStatus, errorIndex)
             else:
                 for varBindTableRow in varBindTable:
                     for name, val in varBindTableRow:
@@ -45,4 +42,35 @@ class pdu_whisperer:
                         #print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
         return pdu_data_list
 
+    def turn_off_outlet(pdu, outlet):
+        _turn_outlet(pdu, outlet, 'off')
+
+    def turn_on_outlet(pdu, outlet):
+        _turn_outlet(pdu, outlet, 'on')
+
+    def _turn_outlet(pdu, outlet, status):
+        """
+        The following sets the value of the outletcontrol action for outlet
+        4 on infeed 1 on tower 2 to "on", effectively turning the outlet on
+        """
+
+        errorIndication, errorStatus, errorIndex, varBinds = cmdGen.setCmd(
+            cmdgen.CommunityData('OSL_private'),
+            cmdgen.UdpTransportTarget((ip_address, self.port)),
+            (cmdgen.MibVariable('Sentry3', 'outletControlAction', 2, 1, 4), status)
+        )
+
+        # Check for errors. If found, raise an exception
+        if errorIndication:
+            raise pdu_exceptions.SnmpActivationError(errorStatus, errorIndex)
+        else:
+            if errorStatus:
+                print('%s at %s' % (
+                    errorStatus.prettyPrint(),
+                    errorIndex and varBinds[int(errorIndex)-1] or '?'
+                    )
+                )
+            else:
+                for name, val in varBinds:
+                    print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
 
