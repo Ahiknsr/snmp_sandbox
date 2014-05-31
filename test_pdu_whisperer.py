140b0e5b1a06a7935d108235af6328eba5e9e9df
Ian Kronquist <iankronquist@osuosl.org>
2014-05-30 18:14:23 -0700

    Add a stub of a test suite

---
+++ test_pdu_whisperer.py
@@ -0,0 +1,18 @@
+import unittest
+import pdu_whisperer
+
+class TestPduWhisperer(unittest.TestCase):
+    def setUp(self):
+        self.whisperer = PduWhisperer()
+
+    def test_get_outlet_power_state(self):
+        pass
+
+    def test_turn_outlet(self):
+
+
+    def tearDown(self):
+        pass
+
+if __name__ = '__main__':
+    unittest.main()
