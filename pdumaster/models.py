ef3bdf1a579d519ad2ef990ad90804d5f884a318
Chance Zibolski <chance.zibolski@gmail.com>
2014-10-21 11:27:21 -0700

    Create methods for get/set commands
    Added methods for checking errors of get/set commands.

--- pdumaster/models.py
+++ pdumaster/models.py
@@ -8,6 +8,7 @@ SENTRY_MIB_DIR = "%s/mib" % os.path.dirname(os.path.realpath(__file__))
 
 
 pdu_addr = 'pdu-b210-dell65.osuosl.oob'
+community_data = 'OSL_private'
 
 Outlet = collections.namedtuple('outlet', ['tower', 'infeed', 'outfeed',], verbose=False)
 
@@ -40,24 +41,35 @@ class SentryPdu(object):
     Power Distribution Unit. Not to be confused with a Protocol Data Unit.
     """
 
-    def __init__(self, cmdgen, addr="localhost", port=161, timeout=1, retries=5):
+    def __init__(self, cmdgen, addr="localhost", port=161, timeout=1, retries=5, community_data=""):
         self.cmdgen = cmdgen
         self.transport = cmdgenerator.UdpTransportTarget((addr, port), retries)
+        self.community_data = cmdgenerator.CommunityData(community_data),
 
-    def _sendGetCommand(self, outlet, command):
-        """Private function to send a given get command name to a pdu"""
-        errorIndication, errorStatus, errorIndex, varBinds = self.cmdgen.getCmd(
-            cmdgenerator.CommunityData('OSL_private'),
-            self.transport,
-            cmdgenerator.MibVariable(
-                'Sentry3',
-                command,
-                outlet.tower,
-                outlet.infeed,
-                outlet.outfeed
-            )
-        )
+    def _check_errors(self, errorIndication, errorStatus, errorIndex, varBinds):
         if errorIndication or errorStatus:
             raise pdu_exceptions.SnmpActivationError(errorStatus, errorIndex)
         else:
             return varBinds
+
+    def outletCommand(self, outlet, command):
+        return cmdgenerator.MibVariable('Sentry3', command, *outlet)
+
+    def _sendSetCommand(self, outlet, command, value):
+        """Private function to send a given set command name to a pdu"""
+        results = self.cmdgen.setCmd(self.community_data, self.transport,
+            (self.outletCommand(outlet, command), value)
+        )
+        return self._check_errors(*results)
+
+    def _sendGetCommand(self, outlet, command):
+        """Private function to send a given get command name to a pdu"""
+        results = self.cmdgen.getCmd(self.community_data, self.transport,
+            self.outletCommand(outlet, command)
+        )
+        return self._check_errors(*results)
+
+    def turn_outlet(self, outlet, status):
+        command = 'outletControlAction'
+        return self._sendSetCommand(outlet, command, status)
+
