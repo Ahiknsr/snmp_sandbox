be514bf4080e7a701baf70c4f3b09ffcc3d77f54
Ahiknsr <gorantla.sai5@gmail.com>
2015-05-15 10:48:29 +0530

    developed pdumaster with ability to view and control outlet states

--- pdumaster/pudmaster.py
+++ pdumaster/pudmaster.py
@@ -24,68 +24,133 @@ mibBuilder.loadModules('SNMPv2-MIB', 'IF-MIB', 'Sentry3')
 # the view controller is handy for viewing objects
 mibViewController = view.MibViewController(mibBuilder)
 
-# a list of pdu ips
-
-# a list of pdu ips
-pdus = ('pdu-b210-dell65.osuosl.oob',)
-
-# standard snmp port
-port = 161
-
-table_headers = ['PDU', 'Field']
-"""
-the following scans all the stuff on each ip address
-"""
-for pdu in pdus:
-
-    errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.nextCmd(
-        cmdgen.CommunityData('OSL_private'),
-        cmdgen.UdpTransportTarget((pdu, port)),
-        cmdgen.MibVariable('Sentry3',''),
-        lookupNames=True, lookupValues=True, lexicographicMode=True,
-        ignoreNonIncreasingOid=True
-    )
-
-    # Check for errors and print out results
-
-    if errorIndication:
-        print(errorIndication)
-    else:
-        if errorStatus:
-            print('%s at %s' % (errorStatus.prettyPrint(),
-                errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'))
-        else:
-            for varBindTableRow in varBindTable:
-                for name, val in varBindTableRow:
-                    modName, symName, suffix = mibViewController.getNodeLocation((name))
-                    print "%s - %s - %s - %s" % (pdu, symName, suffix, val)
-                    print "-------"
-                    #print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+class Pdu():
 
 
-"""
-The following sets the value of the outletcontrol action for outlet
-4 on infeed 1 on tower 2 to "on", effectively turning the outlet on
+    def __init__(self,IP,PORT,ACCESS_STRING):
+        self.ip = IP
+        self.port = PORT
+        self.access_string = ACCESS_STRING
+        self.tower_number = '1.3.6.1.4.1.1718.3.1.4'
+        self.outlet_list =  '1.3.6.1.4.1.1718.3.2.3.1.3'
+        self.outlet_states = '1.3.6.1.4.1.1718.3.2.3.1.5'
 
-
-errorIndication, errorStatus, errorIndex, varBinds = cmdGen.setCmd(
-    cmdgen.CommunityData('OSL_private'),
-    cmdgen.UdpTransportTarget((ip_address, 161)),
-    (cmdgen.MibVariable('Sentry3', 'outletControlAction', 2, 1, 4), 'on')
-)
-
-# Check for errors and print out results
-if errorIndication:
-    print(errorIndication)
-else:
-    if errorStatus:
-        print('%s at %s' % (
-            errorStatus.prettyPrint(),
-            errorIndex and varBinds[int(errorIndex)-1] or '?'
+    
+    def nextCmd(self,oid):
+        errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.nextCmd(
+            cmdgen.CommunityData(self.access_string),
+            cmdgen.UdpTransportTarget((self.ip, self.port)),
+            oid
             )
-        )
-    else:
-        for name, val in varBinds:
-            print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
 
-"""
+        # Check for errors and print out results
+
+        if errorIndication:
+            print(errorIndication)
+            return errorIndication
+        else:
+            if errorStatus:
+                print('%s at %s' % (errorStatus.prettyPrint(),
+                    errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'))
+                return errorStatus
+            else:
+                for varBindTableRow in varBindTable:
+                    for name, val in varBindTableRow:
+                        #print 'oid is ' + str(name) +'  ' + 'value is ' + str(val)
+                        pass
+                return varBindTable
+
+
+    def getCmd(self,oid):
+        errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.getCmd(
+            cmdgen.CommunityData(self.access_string),
+            cmdgen.UdpTransportTarget((self.ip, self.port)),
+            oid
+            )
+
+        # Check for errors and print out results
+
+        if errorIndication:
+            print(errorIndication)
+            return errorIndication
+        else:
+            if errorStatus:
+                print('%s at %s' % (errorStatus.prettyPrint(),
+                    errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'))
+                return errorStatus
+            else:
+                #print varBindTable
+                for oid , value in varBindTable:
+                    #print value
+                    return value
+            
+
+    def setCmd(self,oid,value):
+        errorIndication, errorStatus, errorIndex, varBindTable = cmdGen.setCmd(
+            cmdgen.CommunityData(self.access_string),
+            cmdgen.UdpTransportTarget((self.ip, self.port)),
+            (cmdgen.MibVariable(oid),value)
+            )
+
+        # Check for errors and print out results
+
+        if errorIndication:
+            print(errorIndication)
+            return errorIndication
+        else:
+            if errorStatus:
+                print('%s at %s' % (errorStatus.prettyPrint(),
+                    errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'))
+                return errorStatus
+            else:
+                #print varBindTable
+                for oid , value in varBindTable:
+                    #print oid , value
+                    return value
+
+
+    def get_num_towers(self):
+
+        IP= self.ip
+        PORT= self.port
+
+        for varBindTableRow in self.nextCmd(self.tower_number):
+            for oid , value in varBindTableRow:
+                return value
+
+
+    def get_outlet_details(self):
+
+        outlet_oids = []
+        outlet_name = []
+        for varBindTableRow in self.nextCmd(self.outlet_list):
+            for oid , name in varBindTableRow:
+                outlet_oids.append(str(oid))
+                outlet_name.append(str(name))
+
+        return outlet_oids , outlet_name
+
+
+    def state_from_oid(self,OID):
+
+        temp = OID.split('.')
+        temp[-4] = '5'
+        state_oid = '.'.join(temp)
+        return self.getCmd(state_oid)
+
+
+    def change_state(self,tower,outlet,state):
+
+        tower_dict = { 'A':'1' , 'B':'2' , 'a':'1' , 'b':'2' }
+        state_dict = { 'none': 0 , 'on' : 1 , 'off' : 2 , 'reboot' : 3 } 
+
+        try:
+            
+            oid =  '1.3.6.1.4.1.1718.3.2.3.1.11.' + tower_dict[tower] + '.1.' + str(outlet) 
+            return self.setCmd(oid,state_dict[state])
+
+        except:
+
+            return 'invalid data'
+
+
